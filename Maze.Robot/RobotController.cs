4f256de4e263168439a4e540629680e11472461d
Alexander Wallner <IF130031@HTBLAPerg.onmicrosoft.com>
2017-10-16 19:02:22 +0200

    maze solution

--- Maze.Robot/RobotController.cs
+++ Maze.Robot/RobotController.cs
@@ -1,4 +1,6 @@
 ﻿using Maze.Library;
+using System.Collections.Generic;
+using System.Drawing;
 
 namespace Maze.Solver
 {
@@ -8,7 +10,11 @@ namespace Maze.Solver
     public class RobotController
     {
         private IRobot robot;
-
+        // why hashset?
+        // https://stackoverflow.com/questions/4558754/define-what-is-a-hashset 
+        // -> says it is quick and good for searching 
+        protected HashSet<Point> visited = new HashSet<Point>();
+        protected bool end = false; // he isn't at the end in the beginning...
         /// <summary>
         /// Initializes a new instance of the <see cref="RobotController"/> class
         /// </summary>
@@ -32,14 +38,104 @@ namespace Maze.Solver
         public void MoveRobotToExit()
         {
             // Here you have to add your code
+            Point initpoint = new Point(0, 0);  // Why Point? -> x,y
+            visited = new HashSet<Point>();
 
+            robot.ReachedExit += (_, __) => end = true;
+            // solve maze
+            recursiveVisitField(initpoint);
+            // he returned without solution
+            // our robot trapped :(
+            if (end == false)
+                robot.HaltAndCatchFire(); 
+
+            // not very glorious code
+            //while (!reachedEnd)
+            //{
+            //    if (robot.CanIMove(Direction.Right))
+            //    {
+            //        // it can move right
+            //        robot.Move(Direction.Right); // he moved
+            //    }
+
+            //    if (robot.CanIMove(Direction.Left))
+            //    {
+            //        // it can move left
+            //        robot.Move(Direction.Left); // he moved
+            //    }
+
+            //    if (robot.CanIMove(Direction.Down))
+            //    {
+            //        // it can move down
+            //        robot.Move(Direction.Down); // he moved
+            //    }
+
+            //    if (robot.CanIMove(Direction.Up))
+            //    {
+            //        // it can move up
+            //        robot.Move(Direction.Up); // he moved
+            //    }
+
+            //    if (!robot.CanIMove(Direction.Up) && !robot.CanIMove(Direction.Down) && !robot.CanIMove(Direction.Left) && !robot.CanIMove(Direction.Right))
+            //    {
+            //        // it is trapped :(
+            //        robot.HaltAndCatchFire();
+            //    }
+              //}
             // Trivial sample algorithm that can just move right
-            var reachedEnd = false;
-            robot.ReachedExit += (_, __) => reachedEnd = true;
+            //var reachedEnd = false;
+            //robot.ReachedExit += (_, __) => reachedEnd = true;
 
-            while (!reachedEnd)
+            //while (!reachedEnd)
+            //{
+            //    robot.Move(Direction.Right);
+            //}
+        }
+        /// <summary>
+        /// checks for exit
+        /// </summary>
+        /// <param name="testpoint"></param>
+        public void recursiveVisitField(Point testpoint)
+        {
+            // check if he has never been here and is not at the end
+            if (visited.Contains(testpoint)==false && end==false)
             {
-                robot.Move(Direction.Right);
+                // visited testpoint
+                visited.Add(testpoint); 
+                // he can move left
+                if (robot.TryMove(Direction.Left) == true)
+                {
+                    // check point left
+                    Point newtestpoint = new Point(testpoint.X - 1, testpoint.Y);
+                    // try another point 
+                    recursiveVisitField(newtestpoint);
+                    // move back to origin
+                    if (end == false) { robot.Move(Direction.Right); }
+                }
+
+                if(robot.TryMove(Direction.Right) == true)
+                {
+                    // check point right
+                    Point newtestpoint = new Point(testpoint.X + 1, testpoint.Y);
+                    recursiveVisitField(newtestpoint);
+                    if (end == false) { robot.Move(Direction.Left); }
+                }
+
+                if (robot.TryMove(Direction.Down) == true)
+                {
+                    // check point below
+                    Point newtestpoint = new Point(testpoint.X, testpoint.Y + 1);
+                    recursiveVisitField(newtestpoint);
+                    if (end == false) { robot.Move(Direction.Up); }
+                }
+
+                if (robot.TryMove(Direction.Up) == true)
+                {
+                    // check point above
+                    Point newtestpoint = new Point(testpoint.X, testpoint.Y - 1);
+                    recursiveVisitField(newtestpoint);
+                    if (end == false) { robot.Move(Direction.Down);  }
+                }
             }
         }
     }
